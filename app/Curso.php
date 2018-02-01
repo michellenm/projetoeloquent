6b2a40becdc10ef7e8c563f106b7d77703fff0fc
php 24 <php.24@daphne.cec.ime.usp.br>
2018-02-01 20:45:30 -0200

    primerio eloquent

---
+++ app/Curso.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+// eloquent é uma biblioteca de persistencia de dados - pega as infos do objeto e salva no BD, que o laravael usa.
+// mapeia objeto para tabela, essa tabela recebe o nome da classe no PLURAL( em ingles)
+
+class Curso extends Model  //a classe curso é filha de model,
+{
+    //
+}
---
+++ database/migrations/2018_02_01_214859_cria_tabela_cursos.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CriaTabelaCursos extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        
+        Schema::create('cursos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->char('turma');
+            $table->char('nome');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cursos');
+    }
+}
