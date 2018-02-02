660d83dc464283825cba83ba9fae7544123643d7
php 24 <php.24@daphne.cec.ime.usp.br>
2018-02-02 19:28:18 -0200

    Editora atualizado

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,11 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
 });
+// Rota para url '/editoras'
+Route::get('/editoras', 'EditoraController@index');
+
+// Rota para url 'editoras/1', por exemplo
+Route::get('/editoras/{editora}', 'EditoraController@show');
